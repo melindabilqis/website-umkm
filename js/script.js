5595e2e438f5ae40cb9d31b2ccbce00c86feaead
melindabilqis <melindabilqisqonita@gmail.com>
2026-09-13 08:53:12 +0700

    feat: tambah informasi lokasi UMKM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-  promoButton.textContent = "Promo: Beli 2 gratis tester!";
+  promoButton.textContent = "Selamat Anda Mendapat Diskon 20%";
   console.log("Promo Kopi Nusa berhasil ditampilkan.");
 });
+
+// const informasiButton = document.querySelector("#informasi");
+
+// informasiButton.addEventListener("click", () => {
+//   informasiButton.textContent = 
+//   "Alamat : Jl. Kusuma Bangsa no.9, Panjang Baru, Kec. Pekalongan Utara, Kota Pekalongan, Jawa Tengah 51141." +
+//   "\n" + "BUKA   : Setiap hari pukul 10.00 - 23.00 WIB.";
+//   console.log("Informasi gerai berhasil ditampilkan.");
+// });
