e70a774b060c2fa32bdc0999fb4483681722c294
melindabilqis <melindabilqisqonita@gmail.com>
2026-09-18 21:39:12 +0700

    Menambahkan gambar menu dan memperbarui script

--- js/script.js
+++ js/script.js
@@ -5,11 +5,3 @@ promoButton.addEventListener("click", () => {
   console.log("Promo Kopi Nusa berhasil ditampilkan.");
 });
 
-// const informasiButton = document.querySelector("#informasi");
-
-// informasiButton.addEventListener("click", () => {
-//   informasiButton.textContent = 
-//   "Alamat : Jl. Kusuma Bangsa no.9, Panjang Baru, Kec. Pekalongan Utara, Kota Pekalongan, Jawa Tengah 51141." +
-//   "\n" + "BUKA   : Setiap hari pukul 10.00 - 23.00 WIB.";
-//   console.log("Informasi gerai berhasil ditampilkan.");
-// });
